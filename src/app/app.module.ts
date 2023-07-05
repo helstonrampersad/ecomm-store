8a383ec65396b9b0322102bba8d869dea405cf2d
Helston Rampersad <helstonrampersad@live.com>
2023-07-05 10:36:39 -0400

    made simple login screen in html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { TcgtabComponent } from './tcgtab/tcgtab.component';
-import { CreditcardtabComponent } from './creditcardtab/creditcardtab.component';
-import { CheckoutComponent } from './checkout/checkout.component';
+import { LoginComponent } from './components/login/login.component';
+import { TcgtabComponent } from './components/tcgtab/tcgtab.component';
+import { CreditcardtabComponent } from './components/creditcardtab/creditcardtab.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
 
 @NgModule({
   declarations: [
@@ -14,11 +22,18 @@ import { CheckoutComponent } from './checkout/checkout.component';
     LoginComponent,
     TcgtabComponent,
     CreditcardtabComponent,
-    CheckoutComponent
+    CheckoutComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
