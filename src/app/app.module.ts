e103cffe089471596f715f6eef5b176680f02952
Helston Rampersad <helstonrampersad@live.com>
2023-07-09 18:55:16 -0400

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,41 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { TcgtabComponent } from './components/tcgtab/tcgtab.component';
-import { CreditcardtabComponent } from './components/creditcardtab/creditcardtab.component';
-import { CheckoutComponent } from './components/checkout/checkout.component';
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { BrowserModule } from '@angular/platform-browser';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { CreditcardtabComponent } from './components/creditcardtab/creditcardtab.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
+import { LoginComponent } from './components/login/login.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { NgModule } from '@angular/core';
+import { TcgtabComponent } from './components/tcgtab/tcgtab.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CheckoutComponent,
+    CreditcardtabComponent,
     LoginComponent,
     TcgtabComponent,
-    CreditcardtabComponent,
-    CheckoutComponent,
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSlideToggleModule,
+    BrowserModule,
     FlexLayoutModule,
-    MatFormFieldModule,
-    MatInputModule,
     MatButtonModule,
     MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSlideToggleModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-  
+  loginForm: FormGroup = new FormGroup({
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
+
+  login() {
+    if (this.loginForm.valid) {
+      const username = this.loginForm.get('username')!.value;
+      const password = this.loginForm.get('password')!.value;
+
+      console.log('Username:', username);
+      console.log('Password:', password);
+    }
+  }
 }
